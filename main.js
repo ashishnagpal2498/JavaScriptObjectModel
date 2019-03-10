e96d38c298fd8c0853a5f3eac5745bc96f17eb41
ashish nagpal <ashishnagpal2498@gmail.com>
2019-03-11 02:43:46 +0530

    Bouncing balls- Animation

--- main.js
+++ main.js
@@ -28,3 +28,54 @@ Ball.prototype.draw = function () {
     ctx.arc(this.x,this.y,this.size,0,2*Math.PI) //5 pms - x -
     ctx.fill() ; //End the canvas here
 }
+Ball.prototype.update = function () {
+//    Check if the ball is still inside canvas or not
+    if((this.x + this.size )>=width)
+    {
+        this.velX = -this.velX;
+    }
+    if((this.x - this.size )<=0)
+    {
+        this.velX = -this.velX;
+    }
+    if((this.y + this.size )>=height)
+    {
+        this.velY = -this.velY;
+    }
+    if((this.y - this.size )<=0)
+    {
+        this.velY = -this.velY;
+    }
+    //Co-ordinates are added - according to velocity
+    this.x+=this.velX;
+    this.y+=this.velY;
+}
+
+let balls = [];
+
+function loop() {
+    ctx.fillStyle = "rgba(0,0,0,0.25)";
+    ctx.fillRect(0, 0, width, height);
+//    Creating 25 balls -
+    while(balls.length<25)
+    {   let size = random(5,10);
+        let myb = new Ball(
+            random(0+size,width-size),
+            random(0+size,height-size),
+            random(-7,7),
+            random(-7,7),
+            'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+            size
+        );
+        balls.push(myb);
+    }
+
+//    Creating ball on screen
+    for (var i = 0; i < balls.length; i++) {
+        balls[i].draw();
+        balls[i].update();
+    }
+
+    requestAnimationFrame(loop);
+}
+loop();
